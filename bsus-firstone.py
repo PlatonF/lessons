b1a7a2cfb5849e7487515a37acbc8508d5dd3538
Platon_ <platon.shirokov2005@gmail.com>
2024-01-14 21:38:18 +0300

    Update bsus-firstone.py
    _

--- bsus-firstone.py
+++ bsus-firstone.py
@@ -1,6 +1,10 @@
 #games = input("What are your favorite activites?")
+def input_list(param):
+    input_list = input(f"Input your favorite {param} separetad by spaces")
+    input_list.split(" ")
 
-games = ["Minecraft", "Roblox"]
+
+games = ["Minecraft", "luyfiyff"]
 food = ["pizza", "azzip"]
 favorites = games + food
 print(favorites)
