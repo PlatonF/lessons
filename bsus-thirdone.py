fb1b7df2ae3e90bfcc6cdc95ddd0839663d80883
Platon_ <platon.shirokov2005@gmail.com>
2024-01-20 18:44:20 +0300

    ~~~

---
+++ bsus-thirdone.py
@@ -0,0 +1,4 @@
+name = input("Input your name:").strip().lower().capitalize()
+second_name = input("Input your second name:").strip().lower().capitalize()
+
+print("Hello, %s %s"%(name, second_name))
