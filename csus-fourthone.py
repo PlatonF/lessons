1721ff893b3fb77b2e90830447f8164b3d9a1f4d
Platon_ <platon.shirokov2005@gmail.com>
2024-01-27 18:03:09 +0300

    d

---
+++ csus-fourthone.py
@@ -0,0 +1,11 @@
+ninjas = float(input("How many ninjas?"))
+if ninjas < 10:
+    print("I'll defeat these ninjas!")
+else: 
+    if ninjas < 30:
+        print("It won't be easy, but I'll defeat them")
+    else:
+        if ninjas < 50:
+            print("There are too many of them!")
+        else:
+            print("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
