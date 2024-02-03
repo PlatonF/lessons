b11c948dbcc86da27acc4cae2325767e9e71dacb
Platon_ <platon.shirokov2005@gmail.com>
2024-02-03 17:58:42 +0300

    1

--- dsus-firtone3.py
+++ dsus-firtone3.py
@@ -1,6 +1,6 @@
 w = float(input("age="))
 age = 0
-#range(0, 1)
+#range(0, 12)
 while(age < w):
     age = age + 2
     print(age)
--- dsus-firtone4.py
+++ dsus-firtone4.py
@@ -1,6 +1,6 @@
 w = 12
 age = 0
-#range(0, 1)
+#range(0, 12)
 while(age < w):
     age = age + 2
     print(age)
